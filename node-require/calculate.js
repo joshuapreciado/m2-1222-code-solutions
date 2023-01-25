98d1c4907896fdd8b32f99c97b3f2e1494d8ce0f
joshuapreciado <preciadojoshua340@yahoo.com>
2023-01-25 04:26:12 +0000

    removed parameters & added const to the variables x, test, and y

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -3,10 +3,10 @@ const subtract = require('./subtract');
 const multiply = require('./multiply');
 const divide = require('./divide');
 
-const calculation = (x, test, y) => {
-  x = Number(process.argv[process.argv.length - 3]);
-  test = process.argv[process.argv.length - 2];
-  y = Number(process.argv[process.argv.length - 1]);
+const calculation = () => {
+  const x = Number(process.argv[process.argv.length - 3]);
+  const test = process.argv[process.argv.length - 2];
+  const y = Number(process.argv[process.argv.length - 1]);
   if (test === 'plus') {
     console.log('result:', add(x, y));
   }
