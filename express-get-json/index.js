ad863d12b74f8f5417857444a9c3497814f0927c
joshuapreciado <preciadojoshua340@yahoo.com>
2023-01-31 05:20:55 +0000

    Completed express-get-json

---
+++ express-get-json/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+const app = express();
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
+
+app.get('/api/grades', (req, res) => {
+  const empty = [];
+  for (const key in grades) {
+    empty.push(grades[key]);
+  }
+  res.json(empty);
+});
