809c8cc6c2f3ce3a52be0a3eccb7a7bb0d98c933
joshuapreciado <preciadojoshua340@yahoo.com>
2023-02-03 04:07:32 +0000

    Completed node-notes-cli

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -14,3 +14,21 @@ if (process.argv[2] === 'create') {
     if (err) throw err;
   });
 }
+
+if (process.argv[2] === 'delete') {
+  delete data.notes[process.argv[3]];
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+    if (err) throw err;
+  });
+}
+
+if (process.argv[2] === 'update') {
+  for (const id in data.notes) {
+    if (data.notes[id] === data.notes[process.argv[3]]) {
+      data.notes[process.argv[3]] = process.argv[4];
+    }
+  }
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+    if (err) throw err;
+  });
+}
