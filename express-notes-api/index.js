4ad3d567e64ef8d3c36728216fcfe2d702cbb7c7
joshuapreciado <preciadojoshua340@yahoo.com>
2023-02-08 06:18:50 +0000

    Completed express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const data = require('./data.json');
+const MiddleWare = express.json();
+app.use(MiddleWare);
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
@@ -27,4 +29,75 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(404);
     res.json({ error: 'Unable to find the id' });
   }
+});
+
+// CAN POST A NOTE
+const fs = require('fs');
+app.post('/api/notes/', (req, res) => {
+  if (req.body.content) {
+    res.status(400);
+    res.json({ error: 'Content not found' });
+  }
+  data.notes[data.nextId] = {};
+  data.notes[data.nextId].id = data.nextId;
+  data.notes[data.nextId].content = req.body.content;
+  data.nextId++;
+
+  fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.status(500);
+      res.json({ error: 'Error' });
+    } else {
+      res.status(201);
+      res.json(data.notes[data.nextId - 1], { error: 'Success' });
+    }
+  });
+});
+
+// CAN DELETE A NOTE
+app.delete('/api/notes/:id', (res, req) => {
+  const id = req.params.id;
+  if (parseInt(id) <= 0 || isNaN(id)) {
+    res.status(400).json({ error: 'ID is not positive integer' });
+  }
+  if (data.notes[id] === undefined) {
+    res.status(404).json({ error: 'ID not found' });
+  }
+  delete data.notes[id];
+  fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    } else {
+      res.status(204);
+      res.send();
+    }
+  });
+});
+
+// PUT NOTE
+app.put('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (parseInt(id) <= 0 || isNaN(id)) {
+    res.status(400).json({ error: 'Not a positive integer' });
+  }
+  if (req.body.content) {
+    res.status(400).json({ error: 'Does not have a content property in the request.body' });
+  }
+  if (data.notes[id] === undefined) {
+    res.status(400).json({ error: 'Unable to locate id' });
+  }
+  data.notes[id].content = req.body.content;
+  fs.writeFile('./data.json', JSON.stringify(data, null, 2), err => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      res.status(500).json({ error: 'Error' });
+    } else {
+      res.status(200).json(data.notes[id]);
+    }
+  });
 });
